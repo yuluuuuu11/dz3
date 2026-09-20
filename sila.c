e5c991987a43bcac4e3cbd7c9674717ecac14bdb
julyabityukova <bityukovvvajjjuli@mail.ru>
2026-09-20 14:31:08 +0300

    Добавьте файлы проекта.

---
+++ sila.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float h,m;
+	float g = 9.8;
+
+	
+	printf("Введите массу: ");
+	scanf("%f", &m);
+
+	printf("Введите высоту: ");
+	scanf("%f", &h);
+
+	float F = m * g;
+
+	printf("Сила тяжести равна: %.2f Н", F);
+	return 0;
+}
